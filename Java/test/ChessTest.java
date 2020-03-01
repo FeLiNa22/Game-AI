fdb14f2b6733b451e118f8bd2d2eacf933a6c91a
Raul Patel <raulpatel97@gmail.com>
2020-03-01 17:10:51 +0000

    Finished

--- Java/test/ChessTest.java
+++ Java/test/ChessTest.java
@@ -9,7 +9,7 @@ public class ChessTest {
 
   public static void main(String[] args) {
     ChessAgent p1 = new ChessAgent("AI-1",4);
-    ChessAgent p2 = new ChessAgent("AI-2",6);
+    ChessAgent p2 = new ChessAgent("AI-2",4);
     //Player p2 = new Player("Raul");
     Board board = new Board();
     Game<Move> game = new Game<Move>("Chess", p1, p2, board);
--- Java/test/TicTacToeTest.java
+++ Java/test/TicTacToeTest.java
@@ -7,9 +7,9 @@ import TicTacToe.Player;
 public class TicTacToeTest {
 
   public static void main(String[] args) {
-    //TicTacToeAgent p1 = new TicTacToeAgent("AI-1",10);
-    Player p1 = new Player("Raul");
-    TicTacToeAgent p2 = new TicTacToeAgent("AI-2",4);
+    TicTacToeAgent p2 = new TicTacToeAgent("AI-1",10);
+    //Player p2 = new Player("Raul");
+    TicTacToeAgent p1 = new TicTacToeAgent("AI-2",100);
     Board board = new Board();
     Game<Move> game = new Game<Move>("Chess", p1, p2, board);
     game.run();
